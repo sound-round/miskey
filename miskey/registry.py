b997edd813eb0ce96d9c647f9ecade348726269f
sound-round <yudaev1@gmail.com>
2026-03-03 16:02:19 +0300

    refactor: use translate

--- miskey/registry.py
+++ miskey/registry.py
@@ -5,24 +5,29 @@ from .layouts import (
     EN_TO_RU_SHIFTED,
     RU_TO_EN_BASE,
     RU_TO_EN_SHIFTED,
-    LayoutMap,
     build_layout,
 )
 
-EN_TO_RU_LAYOUT: Final[LayoutMap] = build_layout(EN_TO_RU_BASE, EN_TO_RU_SHIFTED)
-RU_TO_EN_LAYOUT: Final[LayoutMap] = build_layout(RU_TO_EN_BASE, RU_TO_EN_SHIFTED)
+TranslationTable = dict[int, str]
 
-LANGUAGE_LAYOUTS: Final[dict[str, LayoutMap]] = {
-    "en-ru": EN_TO_RU_LAYOUT,
-    "ru-en": RU_TO_EN_LAYOUT,
+EN_TO_RU_TABLE: Final[TranslationTable] = str.maketrans(
+    build_layout(EN_TO_RU_BASE, EN_TO_RU_SHIFTED)
+)
+RU_TO_EN_TABLE: Final[TranslationTable] = str.maketrans(
+    build_layout(RU_TO_EN_BASE, RU_TO_EN_SHIFTED)
+)
+
+LANGUAGE_TABLES: Final[dict[str, TranslationTable]] = {
+    "en-ru": EN_TO_RU_TABLE,
+    "ru-en": RU_TO_EN_TABLE,
 }
 
 
-def get_language_layout(language: str) -> LayoutMap:
+def get_language_table(language: str) -> TranslationTable:
     try:
-        return LANGUAGE_LAYOUTS[language]
+        return LANGUAGE_TABLES[language]
     except KeyError as error:
-        supported_languages = ", ".join(sorted(LANGUAGE_LAYOUTS))
+        supported_languages = ", ".join(sorted(LANGUAGE_TABLES))
         message = (
             f"Unsupported language pair '{language}'. "
             f"Supported language pairs: {supported_languages}"
